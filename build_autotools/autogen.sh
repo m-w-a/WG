11fb3a03776c45596f7e729e906c69eb60d2313a
mostafa <mostafa@localhost>
2010-04-08 18:09:54 +0000

    Ignores.

--- build_autotools/autogen.sh
+++ build_autotools/autogen.sh
@@ -1,1 +1,12 @@
-src/../Autotools/autogen.sh
+#!/bin/sh
+
+case "${OSTYPE}" in
+    darwin*)    TOOLIZER=glibtoolize;;
+    *)          TOOLIZER=libtoolize ;;
+esac
+
+aclocal
+autoheader
+autoconf
+${TOOLIZER}
+automake --add-missing
