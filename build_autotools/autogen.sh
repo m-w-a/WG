9b357641f5d0510c982390c700cb8cbfc2424770
mostafa <mostafa@localhost>
2010-04-10 08:14:34 +0000

    Config enhancements.

--- build_autotools/autogen.sh
+++ build_autotools/autogen.sh
@@ -1,12 +1,1 @@
-#!/bin/sh
-
-case "${OSTYPE}" in
-    darwin*)    TOOLIZER=glibtoolize;;
-    *)          TOOLIZER=libtoolize ;;
-esac
-
-aclocal
-autoheader
-autoconf
-${TOOLIZER}
-automake --add-missing
+src/../Autotools/autogen.sh
