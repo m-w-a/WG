279bc09ce12d292dad4926d3d68b5fb4a08ddb72
Your Name <you@example.com>
2013-05-11 01:15:08 -0700

    Add exception guards to unit test.

--- Utils/Tests/ScopedStdStreamRedirector/ScopedStdStreamRedirectUnitTest.cc
+++ Utils/Tests/ScopedStdStreamRedirector/ScopedStdStreamRedirectUnitTest.cc
@@ -3,61 +3,80 @@
 #include <string>
 #include <gtest/gtest.h>
 #include <WG/Utils/ScopedStdStreamRedirector.hh>
+#include <WG/GTest/Exceptions.hh>
 
 using namespace WG::Utils;
 
 TEST(wg_utils_scopedstdstreamredirector, cout_redirects)
 {
-  std::ostringstream oss;
-  ScopedStdStreamRedirector redirectCout(std::cout, *oss.rdbuf());
-  
-  char testMsg[] = "Hello World!!";
+  try
+  {
+    std::ostringstream oss;
+    ScopedStdStreamRedirector redirectCout(std::cout, *oss.rdbuf());
 
-  std::cout << testMsg;
-  EXPECT_STREQ(oss.str().c_str(), testMsg);
+    char testMsg[] = "Hello World!!";
+  
+    std::cout << testMsg;
+    EXPECT_STREQ(oss.str().c_str(), testMsg);
+  }
+  WG_GTEST_CATCH
 }
 
 TEST(wg_utils_scopedstdstreamredirector, cin_redirects)
 {
-  char testMsg[] = "Hello World!";
-  std::istringstream iss(testMsg);
+  try
+  {
+    char testMsg[] = "Hello World!";
+    std::istringstream iss(testMsg);
 
-  ScopedStdStreamRedirector redirectCin(std::cin, *iss.rdbuf());
+    ScopedStdStreamRedirector redirectCin(std::cin, *iss.rdbuf());
 
-  std::string cinMsgPt1;
-  std::string cinMsgPt2;
-  std::cin >> cinMsgPt1 >> cinMsgPt2;
-  EXPECT_STREQ(cinMsgPt1.c_str(), "Hello");
-  EXPECT_STREQ(cinMsgPt2.c_str(), "World!");
+    std::string cinMsgPt1;
+    std::string cinMsgPt2;
+    std::cin >> cinMsgPt1 >> cinMsgPt2;
+    EXPECT_STREQ(cinMsgPt1.c_str(), "Hello");
+    EXPECT_STREQ(cinMsgPt2.c_str(), "World!");
+  }
+  WG_GTEST_CATCH
 }
 
 TEST(wg_utils_scopedstdstreamredirector, redirects_in_scope_only)
 {
-  std::ostringstream strmToRedirect;
-  
+  try
   {
-    std::ostringstream oss;
-    ScopedStdStreamRedirector redirectOStringStream(strmToRedirect, *oss.rdbuf());
+    std::ostringstream strmToRedirect;
+
+    {
+      std::ostringstream oss;
+      ScopedStdStreamRedirector redirectOStringStream(
+        strmToRedirect,
+        *oss.rdbuf());
+
+      char testMsg[] = "Hello World!";
+
+      strmToRedirect << testMsg;
+      EXPECT_STREQ(oss.str().c_str(), testMsg);
+    }
     
-    char testMsg[] = "Hello World!";
+    char testMsg[] = "Hola Mundo!";
     
     strmToRedirect << testMsg;
-    EXPECT_STREQ(oss.str().c_str(), testMsg);
+    EXPECT_STREQ(strmToRedirect.str().c_str(), testMsg);
   }
-  
-  char testMsg[] = "Hola Mundo!";
-  
-  strmToRedirect << testMsg;
-  EXPECT_STREQ(strmToRedirect.str().c_str(), testMsg);
+  WG_GTEST_CATCH
 }
 
 TEST(wg_utils_scopedstdstreamredirector, nullstreambuf)
 {
-  std::ostringstream strmToRedirect;
-  ScopedStdStreamRedirector redirectToNull(strmToRedirect, getNullStreambuf());
-  
-  char testMsg[] = "Hello World!";
-  
-  strmToRedirect << testMsg;
-  EXPECT_STREQ(strmToRedirect.str().c_str(), "");
+  try
+  {
+    std::ostringstream strmToRedirect;
+    ScopedStdStreamRedirector redirectToNull(strmToRedirect, getNullStreambuf());
+
+    char testMsg[] = "Hello World!";
+
+    strmToRedirect << testMsg;
+    EXPECT_STREQ(strmToRedirect.str().c_str(), "");
+  }
+  WG_GTEST_CATCH
 }
