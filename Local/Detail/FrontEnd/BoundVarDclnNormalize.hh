5cc516b7f9e2eb26c155289107370b23aa8fa4b2
Your Name <you@example.com>
2013-06-05 02:07:47 -0700

    Remove unnecessary debugging aide.

--- Local/Detail/FrontEnd/BoundVarDclnNormalize.hh
+++ Local/Detail/FrontEnd/BoundVarDclnNormalize.hh
@@ -29,34 +29,4 @@
     WG_PP_VARDCLNEXPLICIT_TUPLIZE, \
     WG_PP_VARDCLNIMPLICIT_TUPLIZE) (boundvardcln)
 
-/*
-//Unit Tests.
-#define S1 (T const * ) var1
-#define S2 local(U) var2
-#define S3 localref(Callback &) var3
-#define S4 var4
-#define S5 ref var5
-#define S6 const var6
-#define S7 const ref var7
-
-WG_PP_BOUNDVARDCLN_NORMALIZE(S1)
-WG_PP_BOUNDVARDCLN_NORMALIZE(S2)
-WG_PP_BOUNDVARDCLN_NORMALIZE(S3)
-WG_PP_BOUNDVARDCLN_NORMALIZE(S4)
-WG_PP_BOUNDVARDCLN_NORMALIZE(S5)
-WG_PP_BOUNDVARDCLN_NORMALIZE(S6)
-WG_PP_BOUNDVARDCLN_NORMALIZE(S7)
-
-//EXPECTED:
-//(T const *) (var1)
-//(local(U)) ( var2 )
-//(localref(Callback &)) ( var3 )
-//( BOOST_TYPEOF(var4) ) (var4)
-//( boost::add_reference< BOOST_TYPEOF(var5) >::type ) (var5)
-//( boost::add_const< BOOST_TYPEOF(var6) >::type ) (var6)
-//( boost::add_reference<boost::add_const< BOOST_TYPEOF(var7) >::type>::type ) (var7)
-*/
-
-//TODO.
-
 #endif //WG_PP_BOUNDVARDCLNNORMALIZE_HH_
