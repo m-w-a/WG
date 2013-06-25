6a502f5b83008bd970da46caacdf5f4aeef0f514
Your Name <you@example.com>
2013-06-25 06:02:40 -0700

    More tests.

--- Local/Tests/AutoFunctor/MemBindExplicit.cc
+++ Local/Tests/AutoFunctor/MemBindExplicit.cc
@@ -81,3 +81,94 @@ TEST(wg_autofunctor_membindexplicit, OkIfKeywordThisUBound)
   }
   WG_GTEST_CATCH
 }
+
+TEST(wg_autofunctor_membindexplicit, OkIfLocalTypeNoQualBound)
+{
+  try
+  {
+    struct SomeLocalClass
+    {
+      int value;
+    } localObj = {10};
+
+    static int newValue;
+    newValue = 0;
+    WG_AUTOFUNCTOR(useLocalKeyword, membind (local(SomeLocalClass) localObj) )
+    {
+      localObj.value += 1;
+      newValue = localObj.value;
+    }
+    WG_AUTOFUNCTOR_END;
+
+    EXPECT_EQ(localObj.value, 10);
+    EXPECT_EQ(newValue, 11);
+  }
+  WG_GTEST_CATCH
+}
+
+TEST(wg_autofunctor_membindexplicit, OkIfLocalRefTypeBound)
+{
+  try
+  {
+    struct SomeLocalClass
+    {
+      int value;
+    } localObj = {0};
+
+    WG_AUTOFUNCTOR
+    (useLocalKeyword, membind (local(SomeLocalClass) ref localObj) )
+    {
+      localObj.value = 10;
+    }
+    WG_AUTOFUNCTOR_END;
+
+    EXPECT_EQ(localObj.value, 10);
+  }
+  WG_GTEST_CATCH
+}
+
+TEST(wg_autofunctor_membindexplicit, OkIfLocalConstTypeBound)
+{
+  try
+  {
+    struct SomeLocalClass
+    {
+      int value;
+    } localObj = {10};
+
+    static bool didLocalObjBind;
+    didLocalObjBind = false;
+    WG_AUTOFUNCTOR
+    (useLocalKeyword, membind (local(SomeLocalClass) const localObj) )
+    {
+      didLocalObjBind = (localObj.value == 10);
+    }
+    WG_AUTOFUNCTOR_END;
+
+    EXPECT_TRUE(didLocalObjBind);
+  }
+  WG_GTEST_CATCH
+}
+
+TEST(wg_autofunctor_membindexplicit, OkIfLocalConstRefTypeBound)
+{
+  try
+  {
+    struct SomeLocalClass
+    {
+      int value;
+    } localObj = {10};
+
+    static bool didLocalObjBind;
+    didLocalObjBind = false;
+    WG_AUTOFUNCTOR
+    (useLocalKeyword, membind (local(SomeLocalClass) const ref localObj) )
+    {
+      didLocalObjBind = (localObj.value == 10);
+    }
+    WG_AUTOFUNCTOR_END;
+
+    EXPECT_TRUE(didLocalObjBind);
+  }
+  WG_GTEST_CATCH
+}
