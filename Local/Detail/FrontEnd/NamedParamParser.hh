4ff32969b46c88aca3f1b52315b82e22630a5e1c
Your Name <you@example.com>
2013-08-02 10:33:36 -0700

    Name change for clarity.

--- Local/Detail/FrontEnd/NamedParamParser.hh
+++ Local/Detail/FrontEnd/NamedParamParser.hh
@@ -18,8 +18,8 @@
 //   tokens containing named parameters.
 // specoptions:
 //   any additional options to be passed along.
-// id:
-//   token to uniquely identify macro expansions per module.
+// moduleid:
+//   token to uniquely moduleidentify macro expansions per module.
 // currentkeyword:
 //   the current expected named param.
 // nextkeyword:
@@ -29,23 +29,23 @@
 //   WG_PP_TOKENS_STARTWITH_<currentkeyword>(...) must be defined.
 //
 // Expands to:
-//   1) WG_PP_NAMEDPARAMPARSER_PARSE_FOUND_<id>_<currentkeyword>(
+//   1) WG_PP_NAMEDPARAMPARSER_PARSE_FOUND_<moduleid>_<currentkeyword>(
 //        spec, specoptions, nexttransform)
 //      if the head token of spec matches "currentkeyword", where nexttransform
-//      is the token WG_PP_NAMEDPARAMPARSER_PARSE_<id>_<nextkeyword> , else
-//   2) WG_PP_NAMEDPARAMPARSER_PARSE_NOTFOUND_<id>_<currentkeyword>()
-//      WG_PP_NAMEDPARAMPARSER_PARSE_<id>_<nextkeyword>(spec, specoptions)
+//      is the token WG_PP_NAMEDPARAMPARSER_PARSE_<moduleid>_<nextkeyword> , else
+//   2) WG_PP_NAMEDPARAMPARSER_PARSE_NOTFOUND_<moduleid>_<currentkeyword>()
+//      WG_PP_NAMEDPARAMPARSER_PARSE_<moduleid>_<nextkeyword>(spec, specoptions)
 //-----------------------------------------------------------------------------
 
 #define WG_PP_NAMEDPARAMPARSER_MACRONAME( \
-  spec, id, currentkeyword, nextkeyword) \
+  spec, moduleid, currentkeyword, nextkeyword) \
     WG_PP_NAMEDPARAMPARSER_MACRONAME_IMPL( \
-      spec, id, currentkeyword, nextkeyword)
+      spec, moduleid, currentkeyword, nextkeyword)
 
 #define WG_PP_NAMEDPARAMPARSER_MACROPARAMS( \
-  spec, specoptions, id, currentkeyword, nextkeyword) \
+  spec, specoptions, moduleid, currentkeyword, nextkeyword) \
     WG_PP_NAMEDPARAMPARSER_MACROPARAMS_IMPL( \
-      spec, specoptions, id, currentkeyword, nextkeyword)
+      spec, specoptions, moduleid, currentkeyword, nextkeyword)
 
 //###########
 //Impl Macros
@@ -56,38 +56,38 @@
 //-------------------------------------
 
 #define WG_PP_NAMEDPARAMPARSER_MACRONAME_IMPL( \
-  spec, id, currentkeyword, nextkeyword) \
+  spec, moduleid, currentkeyword, nextkeyword) \
     BOOST_PP_CAT( \
       WG_PP_NAMEDPARAMPARSER_MACRONAME_IMPL, \
       BOOST_PP_CAT(WG_PP_TOKENS_STARTWITH_, currentkeyword) (spec)) \
-    (spec, id, currentkeyword, nextkeyword)
+    (spec, moduleid, currentkeyword, nextkeyword)
 #define WG_PP_NAMEDPARAMPARSER_MACRONAME_IMPL0( \
-  spec, id, currentkeyword, nextkeyword) \
+  spec, moduleid, currentkeyword, nextkeyword) \
     WG_PP_CAT4( \
-      WG_PP_NAMEDPARAMPARSER_PARSE_NOTFOUND_, id, _, currentkeyword) () \
+      WG_PP_NAMEDPARAMPARSER_PARSE_NOTFOUND_, moduleid, _, currentkeyword) () \
     WG_PP_CAT4( \
-      WG_PP_NAMEDPARAMPARSER_PARSE_, id, _, nextkeyword)
+      WG_PP_NAMEDPARAMPARSER_PARSE_, moduleid, _, nextkeyword)
 #define WG_PP_NAMEDPARAMPARSER_MACRONAME_IMPL1( \
-  spec, id, currentkeyword, nextkeyword) \
-    WG_PP_CAT4(WG_PP_NAMEDPARAMPARSER_PARSE_FOUND_, id, _, currentkeyword)
+  spec, moduleid, currentkeyword, nextkeyword) \
+    WG_PP_CAT4(WG_PP_NAMEDPARAMPARSER_PARSE_FOUND_, moduleid, _, currentkeyword)
 
 //----------------------------------
 //WG_PP_NAMEDPARAMPARSER_MACROPARAMS
 //----------------------------------
 
 #define WG_PP_NAMEDPARAMPARSER_MACROPARAMS_IMPL( \
-  spec, specoptions, id, currentkeyword, nextkeyword) \
+  spec, specoptions, moduleid, currentkeyword, nextkeyword) \
     BOOST_PP_CAT( \
       WG_PP_NAMEDPARAMPARSER_MACROPARAMS_IMPL, \
       BOOST_PP_CAT(WG_PP_TOKENS_STARTWITH_, currentkeyword) (spec)) \
-    (spec, specoptions, id, currentkeyword, nextkeyword)
+    (spec, specoptions, moduleid, currentkeyword, nextkeyword)
 #define WG_PP_NAMEDPARAMPARSER_MACROPARAMS_IMPL0( \
-  spec, specoptions, id, currentkeyword, nextkeyword) \
+  spec, specoptions, moduleid, currentkeyword, nextkeyword) \
     (spec, specoptions)
 #define WG_PP_NAMEDPARAMPARSER_MACROPARAMS_IMPL1( \
-  spec, specoptions, id, currentkeyword, nextkeyword) \
+  spec, specoptions, moduleid, currentkeyword, nextkeyword) \
     (spec, \
      specoptions, \
-     WG_PP_CAT4(WG_PP_NAMEDPARAMPARSER_PARSE_, id, _, nextkeyword))
+     WG_PP_CAT4(WG_PP_NAMEDPARAMPARSER_PARSE_, moduleid, _, nextkeyword))
 
 #endif /* WG_PP_NAMEDPARAMPARSER_HH_ */
