1c1b810c95849fb731f82f648505948e9f44f403
Your Name <you@example.com>
2013-12-04 17:17:12 -0800

    Update, document, and clarify.

--- Local/Tests/TestHelper.hh
+++ Local/Tests/TestHelper.hh
@@ -3,6 +3,10 @@
 
 #include <boost/type_traits/remove_all_extents.hpp>
 #include <boost/utility.hpp>
+#include <boost/type_traits/is_same.hpp>
+#include <boost/type_traits/remove_pointer.hpp>
+#include <boost/type_traits/is_const.hpp>
+#include <boost/preprocessor/control/expr_iif.hpp>
 
 //###########
 //Public APIs
@@ -10,16 +14,51 @@
 
 // Needed in order to compare local types.
 // Will produce a compiler error if false.
+// expected:
+// actual:
+//   Some token that expands to a type.
 #define WG_PP_TESTHELPER_IS_SAME_TYPE(expected, actual) \
   WG_PP_TESTHELPER_IS_SAME_TYPE_IMPL(expected, actual)
 
 // Will produce a compiler error if false.
+// expected:
+// actual:
+//   Some token that expands to a variable identifier.
 #define WG_PP_TESTHELPER_IS_SAME_OBJ_NAME(expected, actual) \
   WG_PP_TESTHELPER_IS_SAME_OBJ_NAME_IMPL(expected, actual) \
 
 // Will produce a compiler error if false.
 #define WG_PP_TESTHELPER_MACROEXPANDSTOID(macrotoexpand, expectedid) \
   WG_PP_TESTHELPER_IS_SAME_OBJ_NAME(expectedid, macrotoexpand)
+
+#define WG_TESTHELPER_ASSERT_ISREFERENCE(lvalue1, lvalue2) \
+  EXPECT_TRUE( &(lvalue1) == &(lvalue2) )
+
+#define WG_TESTHELPER_ASSERT_ISNOTREFERENCE(lvalue1, lvalue2) \
+  EXPECT_TRUE( &(lvalue1) != &(lvalue2) )
+
+#define WG_PP_TESTHELPER_ASSERT_ISSAMETYPE(expected, actual) \
+  EXPECT_TRUE(boost::is_same<expected, actual>::value);
+
+// identifier:
+//   Must be a variable identifier, not an expression!.
+//   The type of identifier must not be a local one.
+#define WG_PP_TESTHELPER_ASSERT_ISCONST(identifier) \
+  WG_PP_TESTHELPER_ASSERT_CONST(identifier, 0, 0)
+
+// See WG_PP_TESTHELPER_ASSERT_ISCONST.
+#define WG_PP_TESTHELPER_ASSERT_ISCONST_TPL(identifier) \
+  WG_PP_TESTHELPER_ASSERT_CONST(identifier, 0, 1)
+
+// identifier:
+//   Must be a variable identifier, not an expression!.
+//   The type of identifier must not be a local one.
+#define WG_PP_TESTHELPER_ASSERT_ISNOTCONST(identifier) \
+  WG_PP_TESTHELPER_ASSERT_CONST(identifier, 1, 0)
+
+// See WG_PP_TESTHELPER_ASSERT_ISNOTCONST.
+#define WG_PP_TESTHELPER_ASSERT_ISNOTCONST_TPL(identifier) \
+  WG_PP_TESTHELPER_ASSERT_CONST(identifier, 1, 1)
 
 namespace wg
 {
@@ -45,6 +84,17 @@ typename boost::remove_all_extents<T>::type *
 //######
 //Impls.
 //######
+
+#define WG_PP_TESTHELPER_ASSERT_CONST(identifier, isnotflag, istpl) \
+  EXPECT_TRUE( \
+    BOOST_PP_EXPR_IIF(isnotflag, ! ) \
+    boost::is_const \
+    < \
+      BOOST_PP_EXPR_IIF(istpl, typename) boost::remove_pointer \
+      < \
+        &BOOST_TYPEOF(identifier) \
+      >::type \
+    >::value )
 
 namespace wg
 {
