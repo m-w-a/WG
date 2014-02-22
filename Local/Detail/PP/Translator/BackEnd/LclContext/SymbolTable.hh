ebb469fb480c2e3c4ea60d6e025c2c7f781af294
Your Name <you@example.com>
2014-02-22 11:20:20 -0800

    Update doc interface.

--- Local/Detail/PP/Translator/BackEnd/LclContext/SymbolTable.hh
+++ Local/Detail/PP/Translator/BackEnd/LclContext/SymbolTable.hh
@@ -3,16 +3,13 @@
 
 #include <boost/preprocessor.hpp>
 
-(WG_PP_LCLCONTEXT_SYMBOLTABLE, \
-istpl, \
-isnoex, \
-with_seq, \
-as_seq, \
-with_seq_size)
-
 //################
 //Interface Impls.
+//  (Implements interfaces required by external macros.)
 //################
+
+#define WG_PP_LCLCONTEXT_SYMBOLTABLE_MODULEID(symbtbl) \
+  WG_PP_LCLCONTEXT_ST_GET(symbtbl, MODULEID)
 
 #define WG_PP_LCLCONTEXT_SYMBOLTABLE_ISTPL(symbtbl) \
   WG_PP_LCLCONTEXT_ST_GET(symbtbl, ISPTL)
@@ -20,20 +17,60 @@ with_seq_size)
 #define WG_PP_LCLCONTEXT_SYMBOLTABLE_ISNOEX(symbtbl) \
   WG_PP_LCLCONTEXT_ST_GET(symbtbl, NOEX)
 
-#define WG_PP_LCLCONTEXT_SYMBOLTABLE_TYPESEQ_WITH(symbtbl) \
-  WG_PP_LCLCONTEXT_ST_GET(symbtbl, TYPESEQ_WITH)
+//###########
+//Public APIs
+//###########
 
-#define WG_PP_LCLCONTEXT_SYMBOLTABLE_EXPRSEQ_WITH(symbtbl) \
-  WG_PP_LCLCONTEXT_ST_GET(symbtbl, EXPRSEQ_WITH)
-
-#define WG_PP_LCLCONTEXT_SYMBOLTABLE_TYPESEQ_AS(symbtbl) \
-  WG_PP_LCLCONTEXT_ST_GET(symbtbl, TYPESEQ_AS)
-
-#define WG_PP_LCLCONTEXT_SYMBOLTABLE_OBJSEQ_AS(symbtbl) \
-  WG_PP_LCLCONTEXT_ST_GET(symbtbl, OBJSEQ_AS)
-
-#define WG_PP_LCLCONTEXT_SYMBOLTABLE_XXX_SIZE_WITHAS(symbtbl) \
-  WG_PP_LCLCONTEXT_ST_GET(symbtbl, XXX_SIZE_WITHAS)
+//-----
+//NOTE:
+//-----
+//
+// What ever terms not defined here are defined in the respective BNF.
+//------
+//INPUT:
+//------
+// istpl_noex: { (0|1)(0|1) }
+// withas_dcln_seq:
+//   { BOOST_PP_NIL | { ( with-as-dcln ) }+ }
+// withadhoc_dcln_seq:
+//   { BOOST_PP_NIL | { ( with-adhoc-dcln ) }+ }
+// orderedenteredas_seq:
+//   { ( orderedenteredas-dcln ) }+
+//
+// with-as-dcln :=
+//   normalized-bound-tuple [normalized-bound-tuple]
+// with-adhoc-dcln :=
+//   captured-vars-seq onenter-tuple onexit-tuple
+// orderedenteredas-dcln :=
+//  { (identifier) | (identifier)(identifier)(non-negative-integer) }
+// captured-vars-seq :=
+//   { BOOST_PP_NIL | ( { (normalized-bound-tuple) }+ ) }
+// onenter-tuple :=
+//   compound-statement
+// onexit-tuple :=
+//   compound-statement
+//
+// normalized-bound-tuple := (parsed-explicit-or-deduced-type)(var-name)
+// parsed-explicit-or-deduced-type :=
+//     WG_PP_MARKER_NOOP parsed-explicit-type
+//   | WG_PP_MARKER_DEDUCEDTYPE parsed-deduced-type
+// parsed-explicit-type := parsed-local-type | type explicit-non-local-type
+// parsed-local-type := local(some-token) lib-type-qualifier-seq
+// lib-type-qualifier-seq := (const) | (ref) | (const)(ref)
+// parsed-deduced-type :=
+//   type( { BT | add_const<BT> | add_ref<BT> | add_ref< add_const<BT> > } )
+// BT := BOOST_TYPEOF(some-token)
+//
+//-------
+//OUTPUT:
+//-------
+//A SymbolTable whose values are accessible using the public API.
+#define WG_PP_LCLCONTEXT_SYMBOLTABLE_CREATE( \
+  istpl_noex, \
+  withas_dcln_seq, \
+  withadhoc_dcln_seq, \
+  orderedenteredas_seq) \
+    TODO
 
 //###########
 //Impl Macros
@@ -45,11 +82,15 @@ with_seq_size)
 
 #define WG_PP_LCLCONTEXT_ST_INDX_ISNOEX 2
 
-#define WG_PP_LCLCONTEXT_ST_INDX_TYPESEQ_WITH 3
-#define WG_PP_LCLCONTEXT_ST_INDX_EXPRSEQ_WITH 4
-#define WG_PP_LCLCONTEXT_ST_INDX_TYPESEQ_AS 5
-#define WG_PP_LCLCONTEXT_ST_INDX_OBJSEQ_AS 6
-#define WG_PP_LCLCONTEXT_ST_INDX_XXX_SIZE_WITHAS 7
+#define WG_PP_LCLCONTEXT_ST_INDX_DCLNS_WITHAS 3
+#define WG_PP_LCLCONTEXT_ST_INDX_DCLNS_SIZE_WITHAS 4
+
+#define WG_PP_LCLCONTEXT_ST_INDX_DCLNS_WITHADHOC 5
+#define WG_PP_LCLCONTEXT_ST_INDX_DCLNS_SIZE_WITHADHOC 6
+
+#define WG_PP_LCLCONTEXT_ST_INDX_SEQ_ORDEREDENTEREDAS 7
+
+#define WG_PP_LCLCONTEXT_ST_INDX_DCLNS_TOTALSIZE 8
 
 // suffix: must match one of the following: WG_PP_LCLCONTEXT_ST_INDX_<suffix>
 #define WG_PP_LCLCONTEXT_ST_GET(symbtbl, suffix) \
