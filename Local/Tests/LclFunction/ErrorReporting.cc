db7b6ddf8480deeee4b8868bcf4091254def2c3b
Your Name <you@example.com>
2014-05-18 13:09:32 -0700

    LclFunction unit tests.

--- Local/Tests/LclFunction/ErrorReporting.cc
+++ Local/Tests/LclFunction/ErrorReporting.cc
@@ -17,23 +17,139 @@ TEST(wg_lclfunction_errorreporting, NotOkIfThisURefQualified)
 
 TEST(wg_lclfunction_errorreporting, NotOkIfNamedParameterInvalid)
 {
-  char const * const errMsg1 =
-    BOOST_PP_STRINGIZE
-    (
-      WG_LCLFUNCTION(noop, vrbind (this_) )
-    );
+  // MisspelledNamedParameter
+  {
+    char const * const errMsg =
+      BOOST_PP_STRINGIZE
+      (
+        WG_LCLFUNCTION(noop, vrbind (this_) )
+      );
 
-  char const * const errMsg2 =
-    BOOST_PP_STRINGIZE
-    (
-      WG_LCLFUNCTION(noop, varbind (this_) rAnDM (xyz) )
-    );
+    EXPECT_STREQ(
+      errMsg,
+      "WG_LCLFUNCTION_Error_Invalid_named_parameter");
+  }
 
-  EXPECT_STREQ(
-    errMsg1,
-    "WG_LCLFUNCTION_Error_Invalid_named_parameter");
+  // UnknownNamedParameter
+  {
+    char const * const errMsg =
+      BOOST_PP_STRINGIZE
+      (
+        WG_LCLFUNCTION(noop, varbind (this_) rAnDM (xyz) )
+      );
 
-  EXPECT_STREQ(
-    errMsg2,
-    "WG_LCLFUNCTION_Error_Invalid_named_parameter");
+    EXPECT_STREQ(
+      errMsg,
+      "WG_LCLFUNCTION_Error_Invalid_named_parameter");
+  }
+}
+
+TEST(wg_lclfunction_errorreporting, NotOkIfExplicitlyTypedAndMissingVarName)
+{
+  // VarBind
+  {
+    char const * const errMsg =
+      BOOST_PP_STRINGIZE
+      (
+        WG_LCLFUNCTION(noop, varbind (type(int) ) )
+      );
+
+    EXPECT_STREQ(
+      errMsg,
+      "WG_LCL_Error_missing_variable_name");
+  }
+
+  // VarBind
+  {
+    char const * const errMsg =
+      BOOST_PP_STRINGIZE
+      (
+        WG_LCLFUNCTION(noop, varset (type(int) , 5) )
+      );
+
+    EXPECT_STREQ(
+      errMsg,
+      "WG_LCL_Error_missing_variable_name");
+  }
+}
+
+TEST(wg_lclfunction_errorreporting, NotOkIfImplicitlyTypedAndMissingVarName)
+{
+  // VarBindConst
+  {
+    char const * const errMsg =
+      BOOST_PP_STRINGIZE
+      (
+        WG_LCLFUNCTION(noop, varbind (const ) )
+      );
+
+    EXPECT_STREQ(
+      errMsg,
+      "WG_LCL_Error_missing_variable_name");
+  }
+
+  // VarBindRef
+  {
+    char const * const errMsg =
+      BOOST_PP_STRINGIZE
+      (
+        WG_LCLFUNCTION(noop, varbind (ref ) )
+      );
+
+    EXPECT_STREQ(
+      errMsg,
+      "WG_LCL_Error_missing_variable_name");
+  }
+
+  // VarBindConstRef
+  {
+    char const * const errMsg =
+      BOOST_PP_STRINGIZE
+      (
+        WG_LCLFUNCTION(noop, varbind (const ref ) )
+      );
+
+    EXPECT_STREQ(
+      errMsg,
+      "WG_LCL_Error_missing_variable_name");
+  }
+
+  // VarSetConst
+  {
+    char const * const errMsg =
+      BOOST_PP_STRINGIZE
+      (
+        WG_LCLFUNCTION(noop, varset (const , abc) )
+      );
+
+    EXPECT_STREQ(
+      errMsg,
+      "WG_LCL_Error_missing_variable_name");
+  }
+
+  // VarSetRef
+  {
+    char const * const errMsg =
+      BOOST_PP_STRINGIZE
+      (
+        WG_LCLFUNCTION(noop, varset (ref , xyz) )
+      );
+
+    EXPECT_STREQ(
+      errMsg,
+      "WG_LCL_Error_missing_variable_name");
+  }
+
+  // VarSetConstRef
+  {
+    char const * const errMsg =
+      BOOST_PP_STRINGIZE
+      (
+        WG_LCLFUNCTION(noop, varset (const ref , 123) )
+      );
+
+    EXPECT_STREQ(
+      errMsg,
+      "WG_LCL_Error_missing_variable_name");
+  }
 }
--- Local/Tests/LclFunction/ErrorReportingTpl.cc
+++ Local/Tests/LclFunction/ErrorReportingTpl.cc
@@ -17,23 +17,139 @@ TEST(wg_lclfunction_errorreporting_tpl, NotOkIfThisURefQualified)
 
 TEST(wg_lclfunction_errorreporting_tpl, NotOkIfNamedParameterInvalid)
 {
-  char const * const errMsg1 =
-    BOOST_PP_STRINGIZE
-    (
-      WG_LCLFUNCTION_TPL(noop, vrbind (this_) )
-    );
+  // MisspelledNamedParameter
+  {
+    char const * const errMsg1 =
+      BOOST_PP_STRINGIZE
+      (
+        WG_LCLFUNCTION_TPL(noop, vrbind (this_) )
+      );
 
-  char const * const errMsg2 =
-    BOOST_PP_STRINGIZE
-    (
-      WG_LCLFUNCTION_TPL(noop, varbind (this_) rAnDM (xyz) )
-    );
+    EXPECT_STREQ(
+      errMsg1,
+      "WG_LCLFUNCTION_Error_Invalid_named_parameter");
+  }
 
-  EXPECT_STREQ(
-    errMsg1,
-    "WG_LCLFUNCTION_Error_Invalid_named_parameter");
+  // UnknownNamedParameter
+  {
+    char const * const errMsg2 =
+      BOOST_PP_STRINGIZE
+      (
+        WG_LCLFUNCTION_TPL(noop, varbind (this_) rAnDM (xyz) )
+      );
 
-  EXPECT_STREQ(
-    errMsg2,
-    "WG_LCLFUNCTION_Error_Invalid_named_parameter");
+    EXPECT_STREQ(
+      errMsg2,
+      "WG_LCLFUNCTION_Error_Invalid_named_parameter");
+  }
+}
+
+TEST(wg_lclfunction_errorreporting_tpl, NotOkIfExplicitlyTypedAndMissingVarName)
+{
+  // VarBind
+  {
+    char const * const errMsg =
+      BOOST_PP_STRINGIZE
+      (
+        WG_LCLFUNCTION_TPL(noop, varbind (type(int) ) )
+      );
+
+    EXPECT_STREQ(
+      errMsg,
+      "WG_LCL_Error_missing_variable_name");
+  }
+
+  // VarBind
+  {
+    char const * const errMsg =
+      BOOST_PP_STRINGIZE
+      (
+        WG_LCLFUNCTION_TPL(noop, varset (type(int) , 5) )
+      );
+
+    EXPECT_STREQ(
+      errMsg,
+      "WG_LCL_Error_missing_variable_name");
+  }
+}
+
+TEST(wg_lclfunction_errorreporting_tpl, NotOkIfImplicitlyTypedAndMissingVarName)
+{
+  // VarBindConst
+  {
+    char const * const errMsg =
+      BOOST_PP_STRINGIZE
+      (
+        WG_LCLFUNCTION_TPL(noop, varbind (const ) )
+      );
+
+    EXPECT_STREQ(
+      errMsg,
+      "WG_LCL_Error_missing_variable_name");
+  }
+
+  // VarBindRef
+  {
+    char const * const errMsg =
+      BOOST_PP_STRINGIZE
+      (
+        WG_LCLFUNCTION_TPL(noop, varbind (ref ) )
+      );
+
+    EXPECT_STREQ(
+      errMsg,
+      "WG_LCL_Error_missing_variable_name");
+  }
+
+  // VarBindConstRef
+  {
+    char const * const errMsg =
+      BOOST_PP_STRINGIZE
+      (
+        WG_LCLFUNCTION_TPL(noop, varbind (const ref ) )
+      );
+
+    EXPECT_STREQ(
+      errMsg,
+      "WG_LCL_Error_missing_variable_name");
+  }
+
+  // VarSetConst
+  {
+    char const * const errMsg =
+      BOOST_PP_STRINGIZE
+      (
+        WG_LCLFUNCTION_TPL(noop, varset (const , abc) )
+      );
+
+    EXPECT_STREQ(
+      errMsg,
+      "WG_LCL_Error_missing_variable_name");
+  }
+
+  // VarSetRef
+  {
+    char const * const errMsg =
+      BOOST_PP_STRINGIZE
+      (
+        WG_LCLFUNCTION_TPL(noop, varset (ref , xyz) )
+      );
+
+    EXPECT_STREQ(
+      errMsg,
+      "WG_LCL_Error_missing_variable_name");
+  }
+
+  // VarSetConstRef
+  {
+    char const * const errMsg =
+      BOOST_PP_STRINGIZE
+      (
+        WG_LCLFUNCTION_TPL(noop, varset (const ref , 123) )
+      );
+
+    EXPECT_STREQ(
+      errMsg,
+      "WG_LCL_Error_missing_variable_name");
+  }
 }
