d6f3ecd2e32e3f514f306e42845c5113e9a04749
Your Name <you@example.com>
2014-11-15 14:09:04 -0800

    Add more unit tests.

--- Local/Tests/LclContext/Adhoc.cc
+++ Local/Tests/LclContext/Adhoc.cc
@@ -9,6 +9,23 @@ using namespace ::wg::lclcontext::detail::test;
 
 TEST(wg_lclcontext_adhoc, EnterSpecifiedCompletedScope)
 {
+  bool didCallEnter = false;
+  bool wasScopeCompleted = false;
+
+  WG_LCLCONTEXT(
+    with_adhoc (ref didCallEnter)
+      on_enter( didCallEnter = true )  )
+  {
+    EXPECT_TRUE(didCallEnter);
+    wasScopeCompleted = true;
+  }
+  WG_LCLCONTEXT_END1
+
+  EXPECT_TRUE(wasScopeCompleted);
+}
+
+TEST(wg_lclcontext_adhoc, EnterAndExitSpecifiedCompletedScope)
+{
   RecordKeeper records;
   SimpleScopeMngr scpmngr(ScopeManager::Id0, records);
   Record const & rcd = records.getRecordFor(ScopeManager::Id0);
@@ -30,4 +47,185 @@ TEST(wg_lclcontext_adhoc, EnterSpecifiedCompletedScope)
 
   EXPECT_TRUE(records.isEntryCallOrderCorrect());
   EXPECT_TRUE(records.isExitCallOrderCorrect());
+}
+
+namespace
+{
+
+void returnInducedIncompletedScope(
+  SimpleScopeMngr & scpmngr,
+  Record const & rcd)
+{
+  EXPECT_FALSE(rcd.didCallEnter());
+  WG_LCLCONTEXT(
+    with_adhoc (ref scpmngr)
+      on_enter( scpmngr.enter() )
+      on_exit( scpmngr.exit(scope_completed) )  )
+  {
+    EXPECT_TRUE(rcd.didCallEnter());
+    EXPECT_FALSE(rcd.didCallExit());
+    EXPECT_FALSE(rcd.wasScopeCompleted());
+
+    return;
+  }
+  WG_LCLCONTEXT_END1
+}
+
+}
+TEST(wg_lclcontext_adhoc, EnterAndExitSpecifiedIncompletedScope)
+{
+  // GoToInducedIncompletedScope
+  {
+    RecordKeeper records;
+    SimpleScopeMngr scpmngr(ScopeManager::Id0, records);
+    Record const & rcd = records.getRecordFor(ScopeManager::Id0);
+
+    EXPECT_FALSE(rcd.didCallEnter());
+    WG_LCLCONTEXT(
+      with_adhoc (ref scpmngr)
+        on_enter( scpmngr.enter() )
+        on_exit( scpmngr.exit(scope_completed) )  )
+    {
+      EXPECT_TRUE(rcd.didCallEnter());
+      EXPECT_FALSE(rcd.didCallExit());
+      EXPECT_FALSE(rcd.wasScopeCompleted());
+
+      goto label1;
+    }
+    WG_LCLCONTEXT_END1
+
+    label1:
+
+    EXPECT_TRUE(rcd.didCallExit());
+    EXPECT_FALSE(rcd.wasScopeCompleted());
+
+    EXPECT_TRUE(records.isEntryCallOrderCorrect());
+    EXPECT_TRUE(records.isExitCallOrderCorrect());
+  }
+
+  // ReturnInducedIncompletedScope
+  {
+    RecordKeeper records;
+    SimpleScopeMngr scpmngr(ScopeManager::Id0, records);
+    Record const & rcd = records.getRecordFor(ScopeManager::Id0);
+
+    returnInducedIncompletedScope(scpmngr, rcd);
+
+    EXPECT_TRUE(rcd.didCallExit());
+    EXPECT_FALSE(rcd.wasScopeCompleted());
+
+    EXPECT_TRUE(records.isEntryCallOrderCorrect());
+    EXPECT_TRUE(records.isExitCallOrderCorrect());
+  }
+
+  // BreakInducedIncompletedScope
+  {
+    RecordKeeper records;
+    SimpleScopeMngr scpmngr(ScopeManager::Id0, records);
+    Record const & rcd = records.getRecordFor(ScopeManager::Id0);
+
+    int counter = 1;
+    while(counter > 0)
+    {
+      --counter;
+
+      EXPECT_FALSE(rcd.didCallEnter());
+      WG_LCLCONTEXT(
+        with_adhoc (ref scpmngr)
+          on_enter( scpmngr.enter() )
+          on_exit( scpmngr.exit(scope_completed) ) )
+      {
+        EXPECT_TRUE(rcd.didCallEnter());
+        EXPECT_FALSE(rcd.didCallExit());
+        EXPECT_FALSE(rcd.wasScopeCompleted());
+
+        break;
+      }
+      WG_LCLCONTEXT_END1
+    }
+
+    EXPECT_EQ(0, counter);
+
+    EXPECT_TRUE(rcd.didCallExit());
+    EXPECT_FALSE(rcd.wasScopeCompleted());
+
+    EXPECT_TRUE(records.isEntryCallOrderCorrect());
+    EXPECT_TRUE(records.isExitCallOrderCorrect());
+  }
+
+  // ContinueInducedIncompletedScope
+  {
+    RecordKeeper records;
+    SimpleScopeMngr scpmngr(ScopeManager::Id0, records);
+    Record const & rcd = records.getRecordFor(ScopeManager::Id0);
+
+    int counter = 1;
+    while(counter > 0)
+    {
+      --counter;
+
+      EXPECT_FALSE(rcd.didCallEnter());
+      WG_LCLCONTEXT(
+        with_adhoc (ref scpmngr)
+          on_enter( scpmngr.enter() )
+          on_exit( scpmngr.exit(scope_completed) ) )
+      {
+        EXPECT_TRUE(rcd.didCallEnter());
+        EXPECT_FALSE(rcd.didCallExit());
+        EXPECT_FALSE(rcd.wasScopeCompleted());
+
+        continue;
+      }
+      WG_LCLCONTEXT_END1
+    }
+
+    EXPECT_EQ(0, counter);
+
+    EXPECT_TRUE(rcd.didCallExit());
+    EXPECT_FALSE(rcd.wasScopeCompleted());
+
+    EXPECT_TRUE(records.isEntryCallOrderCorrect());
+    EXPECT_TRUE(records.isExitCallOrderCorrect());
+  }
+
+#ifndef BOOST_NO_EXCEPTIONS
+  // ThrowInducedIncompletedScope
+  {
+    RecordKeeper records;
+    SimpleScopeMngr scpmngr(ScopeManager::Id0, records);
+    Record const & rcd = records.getRecordFor(ScopeManager::Id0);
+
+    struct Exception1 {};
+
+    bool didthrow = false;
+    try
+    {
+      EXPECT_FALSE(rcd.didCallEnter());
+      WG_LCLCONTEXT(
+        with_adhoc (ref scpmngr)
+          on_enter( scpmngr.enter() )
+          on_exit( scpmngr.exit(scope_completed) ) )
+      {
+        EXPECT_TRUE(rcd.didCallEnter());
+        EXPECT_FALSE(rcd.didCallExit());
+        EXPECT_FALSE(rcd.wasScopeCompleted());
+
+        throw Exception1();
+      }
+      WG_LCLCONTEXT_END1
+    }
+    catch(...)
+    {
+      didthrow = true;
+    }
+
+    EXPECT_TRUE(didthrow);
+
+    EXPECT_TRUE(rcd.didCallExit());
+    EXPECT_FALSE(rcd.wasScopeCompleted());
+
+    EXPECT_TRUE(records.isEntryCallOrderCorrect());
+    EXPECT_TRUE(records.isExitCallOrderCorrect());
+  }
+#endif // BOOST_NO_EXCEPTIONS
 }
